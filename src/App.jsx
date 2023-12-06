ddd11ced9f4bb529c5cb63d98ff8a3e6f241b9e8
Isabel621 <isabelaprilia0412@gmaol.com>
2023-12-06 15:24:32 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login'
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
